242ed324f0acf763acf2850d8e1c269e4058d96b
Adarsh Jha <56366603+Adarshjha112@users.noreply.github.com>
2022-03-14 12:53:29 +0530

    first project

---
+++ FirstProject/src/com/jha/Add.java
@@ -0,0 +1,13 @@
+package com.jha;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int m=6,n=5;
+		int sum =m+n;
+		System.out.println("the sum is "+sum);
+		
+	}
+
+}
